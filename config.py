548a7f44151af948753449261f465c05572e24de
Alberto Moreno <amoreno@goumh.umh.es>
2018-04-26 14:14:55 +0200

    .gitignore updated to exclude .idea and ansible galaxy info

--- config.py
+++ config.py
@@ -5,4 +5,3 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SERVER_NAME = "localhost:5080"
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    WTF_CSRF_ENABLED = False
