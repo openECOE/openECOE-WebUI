801c09e573bcf1f231006dc6b240e8dc4f4b4a93
Antonio <antonio.angosto@goumh.umh.es>
2018-04-27 14:06:56 +0200

    added potion-client on requirements, configured server api route, added call to ecoe api

--- app/ui_admin/routes.py
+++ app/ui_admin/routes.py
@@ -1,7 +1,22 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, current_app
 from app.ui_admin import bp
 from app.ui_admin.forms import LoginForm
+from potion_client import Client
+from potion_client.auth import HTTPBearerAuth
+from potion_client.exceptions import ItemNotFound
+from config import Config
 
+
+
+@bp.route('/', methods=['GET'])
+def home():
+
+    api_route = Config.API_ROUTE
+
+    client = Client(api_route)
+    ecoe = client.Ecoe.instances()
+
+    return render_template('admin.html', ecoes=ecoe)
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -12,3 +27,11 @@ def login():
         )
         return redirect(url_for('index'))
     return render_template('login.html',  title='Sign In', form=form)
+
+@bp.route('/eval', methods=['GET'])
+def eval():
+    # stations = client.Station.instances()
+    # for sta in stations:
+    #     print("{} yee")
+
+    return render_template('evaluation.html', title='Evaluación', ecoeName='ECOE 1', stations=["Sta 1", "Sta 2", "Sta 3", "Sta 4", "Sta 5"])
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SERVER_NAME = "localhost:5080"
+    SERVER_NAME = "www.openecoe.com:5080"
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     WTF_CSRF_ENABLED = False
+    API_ROUTE = "http://10.1.56.83:5000/api"
