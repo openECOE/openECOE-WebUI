5287a8cff0f935ee2ff8081d2b1735faf0f3dba2
FernandoRuedas <fernando.ruedas@goumh.umh.es>
2018-05-08 10:35:35 +0200

    updated evaladmin interface updated evaladmin function in routes

--- app/ui_evaluation/routes.py
+++ app/ui_evaluation/routes.py
@@ -15,34 +15,29 @@ def index():
     return render_template('index.html', title='Home',  ecoes=ecoe)
 
 
-@bp.route('/evaluacion')
+@bp.route('/evaluacion', methods=['GET'])
+@bp.route('/evaluacion/<int:i>', methods=['GET'])
 def evaluacion():
     return render_template('evaluacion.html')
 
 @bp.route('/evaladmin/', methods=['GET'])
 @bp.route('/evaladmin/<int:id_ecoe>', methods=['GET'])
 def evaladmin(id_ecoe):
-
-
     ecoe = client.Ecoe(id_ecoe)
     station = client.Station.instances(where={"ecoe": ecoe})
-    days = client.Day.instances(where={"ecoe": ecoe})
+    shifts = client.Shift.instances(where={"ecoe": ecoe})
 
-    days_array=[]
-    for day in days:
-        shifts_array = []
-        shifts = client.Shift.instances(where={"day": day})
-        for shift in shifts:
-            rounds_array = []
-            rounds = client.Round.instances(where={"shift": shift})
-            for round in rounds:
-                rounds_array.append({"round": round.description})
+    shifts_array = []
+    for shift in shifts:
+        planners = client.Planner.instances(where={"shift": shift})
+        rounds_array = []
+        for planner in planners:
+            round = client.Round(planner.round.id)
+            rounds_array.append(round.description)
 
-            shifts_array.append({"shift": shift.start_time, "rounds": rounds_array})
+        shifts_array.append({'shift': shift.time_start, 'rounds': rounds_array})
 
-        days_array.append({'day': day.date, "shifts": shifts_array})
-
-    return render_template('evaladmin.html', ecoe=ecoe, id_ecoe=id_ecoe, stations=station, days=days_array )
+    return render_template('evaladmin.html', ecoe=ecoe, id_ecoe=id_ecoe, stations=station, planner=shifts_array )
 
 
 # @bp.route('/evaluacion/<int: id>', methods=['GET'])
