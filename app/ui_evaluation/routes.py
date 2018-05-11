adff3517b8c4d7c2c51c7cb5eda2f1839b2b3efd
FernandoRuedas <fernando.ruedas@goumh.umh.es>
2018-05-11 12:15:23 +0200

    Added new interface to evaluation Created get routes for evaluation

--- app/ui_evaluation/routes.py
+++ app/ui_evaluation/routes.py
@@ -1,38 +1,48 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, current_app
+from flask_login import login_required, current_user
 from app.ui_evaluation import bp
-from potion_client import Client
-from potion_client.auth import HTTPBearerAuth
 from potion_client.exceptions import ItemNotFound
-from config import Config
-
-api_route = Config.API_ROUTE
-client = Client(api_route)
+from flask import request
 
 @bp.route('/')
 @bp.route('/index')
+@login_required
 def index():
-    ecoe = client.Ecoe.instances()
+    ecoe = current_user.api_client.Ecoe.instances()
     return render_template('index.html', title='Home',  ecoes=ecoe)
 
 
 @bp.route('/evaluacion', methods=['GET'])
-@bp.route('/evaluacion/<int:i>', methods=['GET'])
-def evaluacion():
-    return render_template('evaluacion.html')
+@bp.route('/evaluacion/<int:id_ecoe>/<int:id_station>', methods=['GET'])
+@login_required
+def evaluacion(id_ecoe, id_station):
+    ecoe = current_user.api_client.Ecoe(id_ecoe)
+    station = current_user.api_client.Station(id_station)
+    qblocks = station.qblocks()
+
+    questions_array = []
+    for qblock in qblocks:
+        questions = qblock.questions()
+        for question in questions:
+            options =  current_user.api_client.Option.instances(where = {"question": question})
+            questions_array.append({'question': question,  'options': options})
+
+    return render_template('evaluacion.html', ecoe=ecoe, station = station, qblock=qblocks, questions=questions_array)
 
 @bp.route('/evaladmin/', methods=['GET'])
-@bp.route('/evaladmin/<int:id_ecoe>', methods=['GET'])
+@bp.route('/evaladmin/<int:id_ecoe>/', methods=['GET'])
+@login_required
 def evaladmin(id_ecoe):
-    ecoe = client.Ecoe(id_ecoe)
-    station = client.Station.instances(where={"ecoe": ecoe})
-    shifts = client.Shift.instances(where={"ecoe": ecoe})
+    ecoe = current_user.api_client.Ecoe(id_ecoe)
+    station = current_user.api_client.Station.instances(where={"ecoe": ecoe})
+    shifts = current_user.api_client.Shift.instances(where={"ecoe": ecoe})
 
     shifts_array = []
     for shift in shifts:
-        planners = client.Planner.instances(where={"shift": shift})
+        planners = current_user.api_client.Planner.instances(where={"shift": shift})
         rounds_array = []
         for planner in planners:
-            round = client.Round(planner.round.id)
+            round = current_user.api_client.Round(planner.round.id)
             rounds_array.append(round.description)
 
         shifts_array.append({'shift': shift.time_start, 'rounds': rounds_array})
--- config.py
+++ config.py
@@ -6,10 +6,10 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SERVER_NAME = "www.openecoe.com:5080"
+    SERVER_NAME = "localhost:5080"
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     WTF_CSRF_ENABLED = False
-    API_HOST = "http://10.1.56.94:5000/api"
+    API_HOST = "10.1.56.94:5000"
     API_ROUTE = "http://" + API_HOST + "/api"
     API_AUTH_TOKEN = "http://" + API_HOST + "/auth/tokens"
     LANGUAGES = ['en', 'es']
